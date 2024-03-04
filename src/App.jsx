1dcdbcf0e02cd2e962aa6c487bfe993991a8c71c
Ayaan <ayaan2536@gmail.com>
2024-03-04 12:29:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+function App() {
+  return (
+    <>
+      <div className="contain-div box-border px-5 container mx-auto xs:max-w-[600px] sm:px-0 sm:max-w-[560px] md:px-10 md:max-w-[1360px] 1xl:px-0 2xl:max-w-[1600px]">
+        <div className="navbar mt-12 mb-20 px-6 py-3 rounded-[45px] bg-light-blue flex flex-row justify-between w-[700px] mx-auto bg-opacity-40">
+          <div className="logo flex flex-row items-center">
+            <span class="material-symbols-outlined text-dark">
+              sports_soccer
+            </span>
+            <p className="text-dark font-semibold ml-1 text-lg">AI Insighter</p>
+          </div>
+          <div className="nav-itmes flex flex-row">
+            <p className="text-dark py-1.5 px-4 bg-light rounded-3xl font-semibold text-lg">
+              Home
+            </p>
+            <p className="text-dark py-1.5 px-4 font-semibold text-lg">
+              Explore
+            </p>
+            <p className="text-dark py-1.5 px-4 font-semibold text-lg">
+              Projects
+            </p>
+          </div>
+        </div>
+        <div className="hero-sec grid grid-cols-2">
+          <div className=""></div>
+          <div></div>
+        </div>
+        <div className="contain-grid"></div>
+        <div className="footer"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
